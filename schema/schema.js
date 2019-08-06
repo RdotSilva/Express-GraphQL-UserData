988a59fe8a5ff3114af8cbce08da951b134056a8
Ryan Silva <RyandotSilva@gmail.com>
2019-08-06 14:58:22 -0400

    Add RootQuery

--- schema/schema.js
+++ schema/schema.js
@@ -9,3 +9,18 @@ const UserType = new GraphQLObjectType({
 		age: { type: GraphQLInt }
 	}
 });
+
+const RootQuery = new GraphQLObjectType({
+	name: "RootQueryType",
+	fields: {
+		// If looking for a user
+		user: {
+			// Will give you back a User
+			type: UserType,
+			// Give me an ID
+			args: { id: { type: GraphQLString } },
+			// Resolve reaches out and grabs the data.
+			resolve(parentValue, args) {}
+		}
+	}
+});
