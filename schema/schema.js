e2b9aad06a5d1a27948c3fedfdc9d491cbb31371
Ryan Silva <RyandotSilva@gmail.com>
2019-08-07 18:13:07 -0400

    Add anon functions to fields

--- schema/schema.js
+++ schema/schema.js
@@ -10,25 +10,24 @@ const axios = require("axios");
 
 const CompanyType = new GraphQLObjectType({
 	name: "Company",
-	fields: {
+	fields: () => ({
 		id: { type: GraphQLString },
 		name: { type: GraphQLString },
 		description: { type: GraphQLString },
 		users: {
 			type: new GraphQLList(UserType),
 			resolve(parentValue, args) {
-				console.log("Ryan Debug: resolve -> parentValue", parentValue);
 				return axios
-					.get(`http://localhost:3000/companies/${parentValue.id}`)
+					.get(`http://localhost:3000/companies/${parentValue.id}/users`)
 					.then(res => res.data);
 			}
 		}
-	}
+	})
 });
 
 const UserType = new GraphQLObjectType({
 	name: "User",
-	fields: {
+	fields: () => ({
 		id: { type: GraphQLString },
 		firstName: { type: GraphQLString },
 		age: { type: GraphQLInt },
@@ -40,7 +39,7 @@ const UserType = new GraphQLObjectType({
 					.then(res => res.data);
 			}
 		}
-	}
+	})
 });
 
 const RootQuery = new GraphQLObjectType({
